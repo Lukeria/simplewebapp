f4cc4e1581a4431da8ffc513383c05472e344945
karina <lkarina30082000@gmail.com>
2021-10-18 11:17:44 +0300

    replace DatabaseLoader with schema.sql and data.sql for auto initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee(employee_id, first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES (1, 'Karina', 'Lukashevich', 2, 'Developer', 'FEMALE', '2000-08-30'),
+       (2, 'Peter', 'Petrov', 2, 'Developer', 'MALE', '1980-01-05');
